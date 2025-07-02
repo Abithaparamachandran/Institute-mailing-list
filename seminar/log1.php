93cf1c23ca01f17d6a2ebb1dad4f7d31f0b591e9
abi <165017113+Abithaparamachandran@users.noreply.github.com>
2025-07-02 14:08:06 +0530

    Delete seminar directory

--- seminar/log1.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-$_SESSION['usernamee'] =$_POST['username'];
-?>
-
-<font color="red">Unauthorized users cannot access this page</font>
-
-<a href="index.php">Click to login</a>
-
